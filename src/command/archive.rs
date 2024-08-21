8edfdfdd65151c6bbdcf7b839248b13935ce02a4
Lucas Meier <lucas@cronokirby.com>
2024-08-20 20:30:28 -0700

    Refactor archival command to have more coherent pieces
    This is now split into 3 conceptual stages, which leads to much cleaner
    code.

--- src/command/archive.rs
+++ src/command/archive.rs
@@ -5,6 +5,18 @@ use crate::{cometbft, storage::Storage};
 
 const REINDEXER_FILE_NAME: &'static str = "reindexer_archive.bin";
 
+// # Organization
+//
+// The data flow for this file is:
+//  Archive -> ParsedCommand -> Archiver.
+//
+// First, we transform the user provided options into direct, unambiguous information.
+// For example, figuring out the actual paths where data is stored, having used home directories
+// and overrides as the user specified.
+//
+// Then, we read information in order to prepare the data we need to archive, and the storage
+// we'll archive the data to.
+
 #[derive(clap::Parser)]
 pub struct Archive {
     /// A starting point for reading and writing penumbra data.
@@ -70,45 +82,106 @@ impl Archive {
 
     /// Create or add to our full historical archive of blocks.
     pub async fn run(self) -> anyhow::Result<()> {
-        let archive_file = self.archive_file()?;
-        let archive = Storage::new(Some(&archive_file)).await?;
-
         let cometbft_dir = self.cometbft_dir()?;
-        let config = cometbft::Config::read_dir(&cometbft_dir)?;
-        let genesis = cometbft::Genesis::read_cometbft_dir(&cometbft_dir, &config)?;
+        let archive_file = self.archive_file()?;
+        ParsedCommand::new(cometbft_dir, archive_file).run().await
+    }
+}
 
-        let mut store = cometbft::Store::new(&cometbft_dir, &config)?;
+/// This represents the result of performing a bit of parsing of the command.
+///
+/// We need to reduce some of the redundant options into a more direct set of information.
+struct ParsedCommand {
+    /// The directory where cometbft information is stored.
+    cometbft_dir: PathBuf,
+    /// The file to store our archive database.
+    archive_file: PathBuf,
+}
 
-        let (store_start, store_end) = match (store.first_height(), store.last_height()) {
-            (None, _) | (_, None) => {
-                tracing::info!("empty block store, returning");
-                return Ok(());
-            }
+impl ParsedCommand {
+    pub fn new(cometbft_dir: PathBuf, archive_file: PathBuf) -> Self {
+        Self {
+            cometbft_dir,
+            archive_file,
+        }
+    }
+
+    pub async fn run(self) -> anyhow::Result<()> {
+        let config = cometbft::Config::read_dir(&self.cometbft_dir)?;
+        let genesis = cometbft::Genesis::read_cometbft_dir(&self.cometbft_dir, &config)?;
+        let store = cometbft::Store::new(&self.cometbft_dir, &config)?;
+        let archive = Storage::new(Some(&self.archive_file)).await?;
+
+        Archiver::new(genesis, store, archive).run().await
+    }
+}
+
+/// Responsible for actually running the archival process.
+///
+/// This is a bit of an OOP verb-object, but it serves the purpose of organizing
+/// the information needed
+struct Archiver {
+    /// The genesis information we need to place in the archive.
+    genesis: cometbft::Genesis,
+    /// The store of cometbft information.
+    store: cometbft::Store,
+    /// The place where our archive resides.
+    archive: Storage,
+}
+
+impl Archiver {
+    pub fn new(genesis: cometbft::Genesis, store: cometbft::Store, archive: Storage) -> Self {
+        Self {
+            genesis,
+            store,
+            archive,
+        }
+    }
+
+    /// Retreive the bounds we need to archive between
+    async fn bounds(&mut self) -> anyhow::Result<Option<(u64, u64)>> {
+        let (store_start, store_end) = match (self.store.first_height(), self.store.last_height()) {
+            (None, _) | (_, None) => return Ok(None),
             (Some(start), Some(end)) => (start, end),
         };
 
-        let archive_end = archive.last_height().await?;
+        let archive_end = self.archive.last_height().await?;
 
         let start = std::cmp::max(store_start, archive_end.unwrap_or(0) + 1);
         let end = store_end;
-        // If the end is less than the start, that's odd, and we don't want to just do nothing.
-        anyhow::ensure!(
-            end >= start,
-            "attempting to archive blocks {}..{}",
-            start,
-            end
-        );
+        return Ok(Some((start, end)));
+    }
+
+    async fn archive_genesis(&self) -> anyhow::Result<()> {
         tracing::info!(
-            initial_height = genesis.initial_height(),
+            initial_height = self.genesis.initial_height(),
             "archiving genesis"
         );
-        archive.put_genesis(&genesis).await?;
+        self.archive.put_genesis(&self.genesis).await?;
+        Ok(())
+    }
+
+    pub async fn run(mut self) -> anyhow::Result<()> {
+        self.archive_genesis().await?;
+
+        let (start, end) = match self.bounds().await? {
+            None => {
+                tracing::info!("empty archival range, returning");
+                return Ok(());
+            }
+            Some((x, y)) if y < x => {
+                tracing::info!("empty archival range, returning");
+                return Ok(());
+            }
+            Some(x) => x,
+        };
 
         tracing::info!("archiving blocks {}..{}", start, end);
         for height in start..end {
             tracing::debug!("archiving block {}", height);
 
-            let block = store
+            let block = self
+                .store
                 .block_by_height(height)?
                 .ok_or(anyhow!("missing block at height {}", height))?;
 
@@ -119,7 +192,7 @@ impl Archive {
                 height
             );
 
-            archive.put_block(&block).await?;
+            self.archive.put_block(&block).await?;
             break;
         }
         Ok(())
