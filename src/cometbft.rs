adfa3ec5c34a329f113f79d717de67aaaf47813e
Lucas Meier <lucas@cronokirby.com>
2025-04-07 14:15:37 -0700

    Allow regen to follow a remote node
    This augments the regen command to take a `--follow` input,
    which will allow it to run *past* the blocks in the archive, using
    the remote node as a source of new blocks. These blocks will be
    put inside the archive as well.
    
    This command will first exhaust the archive before fetching remote blocks.
    
    If there's no end block, this will poll the remote store to run forever.

--- src/cometbft.rs
+++ src/cometbft.rs
@@ -309,13 +309,13 @@ impl TryFrom<Value> for Genesis {
 }
 
 /// A stream of blocks, with their heights, allowing for errors.
-pub type BlockStream<'a> = Pin<Box<dyn Stream<Item = anyhow::Result<(u64, Block)>> + 'a>>;
+pub type BlockStream<'a> = Pin<Box<dyn Stream<Item = anyhow::Result<(u64, Block)>> + 'a + Send>>;
 
 /// A trait abstracting over a store of cometbft data.
 ///
 /// This unifies stores that read local data, and stores that read remote data.
 #[async_trait]
-pub trait Store: Send + 'static {
+pub trait Store: Sync + Send + 'static {
     /// Get the genesis file for this Generation.
     async fn get_genesis(&self) -> anyhow::Result<Genesis>;
     /// Get the height bounds for this Generation.
